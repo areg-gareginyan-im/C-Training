d7855a779353030c7863b20dcba63e5083994410
Arman Kostandyan <arman_k@instigatemobile.com>
2019-04-19 14:31:44 +0400

    Updated array elements type to double

--- bubble-sort/main.c
+++ bubble-sort/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int len);
+void bubbleSort(double arr[], int len);
 
 int main()
 {
-  int numbers[] = {-2, 4, -3, 2, 1, -2};
+  double numbers[] = {-2.65, 4.0, -3, 2.65, 2.64, -2};
 
   int len = (int) sizeof(numbers) / sizeof(numbers[0]);
 
@@ -12,15 +12,15 @@ int main()
 
   for (int i = 0; i < len; i++)
   {
-    printf("index: %d, number: %d\n", i, numbers[i]);
+    printf("index: %d, number: %f\n", i, numbers[i]);
   }
 
   return 0;
 }
 
-void bubbleSort(int arr[], int len)
+void bubbleSort(double arr[], int len)
 {
-  int tmp;
+  double tmp;
   for (int j = 0; j < len - 1; j++)
   {
     for (int i = 0; i < len - j - 1; i++)
