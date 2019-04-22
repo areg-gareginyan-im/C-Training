30ebb20eea3d1b18b62a1c29e4a5ba9fbdef1ab3
Arman Kostandyan <arman_k@instigatemobile.com>
2019-04-22 15:59:28 +0400

    Handled the case of no free space

--- binary-search-tree/tree/node/node.c
+++ binary-search-tree/tree/node/node.c
@@ -3,7 +3,10 @@
 
 struct node* newNode(int data) {
   struct node *elem = (struct node*) malloc(sizeof(struct node));
-
+  if(elem == NULL) {
+    printf("ERROR: Out of memory.");
+    return NULL;
+  }
   elem->data = data;
   elem->left = NULL;
   elem->right = NULL;
