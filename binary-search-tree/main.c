53af70b5f739e66299697e9f1629d2de5385709d
Arman Kostandyan <arman_k@instigatemobile.com>
2019-04-22 15:40:31 +0400

    Added functionality to push random numbers into Binary tree

--- binary-search-tree/main.c
+++ binary-search-tree/main.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 #include "./tree/tree.h"
 
-void pushRandomIntegers() {
-
-}
+/**
+ * pushes random integers with [0,20] range
+ */
+void pushRandomIntegers(struct node *root, unsigned int count);
 
 int main() {
-	struct node* root = NULL;
-	int val = 0;
+	struct node *root = NULL;
+	int val = 8;
 	root = put(root, 5);
-	pushRandomIntegers(root);
-	put(root, 15);
-	put(root, -2);
-	put(root, 1);
-	put(root, -21);
-	put(root, 150);
-	put(root, -26);
-	/** TODO: Add new elements */
+	pushRandomIntegers(root, 20);
 
 	printf("Root: %d\n", root->data);
-	printf("Root left: %d\n", root->left->data);
-	printf("Root right: %d\n", root->right->data);
 
-	printf("Root right: %d\n", root->right->data);
-	printf("root->left->left->left: %d\n", root->left->left->left->data);
-	printf("root->left->right: %d\n", root->left->right->data);
-
-	printf("%d is found in Tree: %s\n", val, find(root, val) ? "true": "false");
   print(root, 4);
+	printf("\n%d is found in Tree: %s\n", val, find(root, val) ? "true": "false");
 	printf("Press Enter to exit...");
 	getchar();
 	return 0;
+}
+
+void pushRandomIntegers(struct node *root, unsigned int count) {
+	srand(time(NULL));
+	while(count--) {
+		put(root, rand() % 20);
+	}
 }
